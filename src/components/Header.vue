4ae8cd17949c2136a969161578f3af8576960e67
LorkiVG <lorkiofficial@gmail.com>
2024-06-12 04:24:40 +0300

    Update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="header">
-        <img src="/src/assets/img/components/Header/logo/white.svg" class="header__logo">
         <div class="header__content">
-            <img src="/src/assets/img/components/Header/logo/white.svg" class="header__content__logo"> 
-            <div class="header__content__links">
-                <a href="#" class="header__content__links__link">Services</a>
-                <a href="#" class="header__content__links__link">Projects</a>
-                <a href="#" class="header__content__links__link">Reviews</a>
-                <a href="#" class="header__content__links__link">Contacts</a>
-            </div>
+            <img src="/src/assets/img/components/Header/logo/white.svg" class="header__content__logo">
+            <span></span>
+            <nav class="header__content__nav">
+                <a href="#" class="header__content__nav__link">Services</a>
+                <a href="#" class="header__content__nav__link">Projects</a>
+                <a href="#" class="header__content__nav__link">Reviews</a>
+                <a href="#" class="header__content__nav__link">Contacts</a>
+            </nav>
             <label class="header__content__switch">
                 <input type="checkbox" id="theme__switcher" class="header__content__switch__checkbox">
                 <span class="header__content__switch__round"></span>
-            </label>
+             </label>
+             <span></span>
             <button class="header__content__button">Propose project</button>
+            <div class="header__content__burger">
+                <div class="header__content__burger__lines"></div>
+            </div>
         </div>
-        <div class="header__burger">
-            <div class="header__burger__lines"></div>
-        </div>
+        
     </div>
 </template>
 
