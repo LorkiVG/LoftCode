307894360ff706727e614950c156080433f515ee
LorkiVG <lorkiofficial@gmail.com>
2024-06-14 02:59:23 +0300

    Update

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,13 +13,6 @@
                     </div>
                     <img src="/src/assets/img/components/Projects/ikpay.png" class="projects__content__projects__project__img">
                 </div>
-                <div class="projects__content__projects__project center">
-                    <div class="projects__content__projects__project__info">
-                        <p class="projects__content__projects__project__info__title">Premodern</p>
-                        <p class="projects__content__projects__project__info__category">Design, Development</p>
-                    </div>
-                    <img src="/src/assets/img/components/Projects/laslesvpn.png" class="projects__content__projects__project__img">
-                </div>
                 <div class="projects__content__projects__project right">
                     <div class="projects__content__projects__project__info">
                         <p class="projects__content__projects__project__info__title">Langing page LaslesVPN app</p>
