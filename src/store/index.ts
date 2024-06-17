079055d2b98b9a6fdf515786feb7b066e29f0ae7
LorkiVG <lorkiofficial@gmail.com>
2024-06-18 02:04:54 +0300

    Update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,16 @@
 import { createStore } from 'vuex';
 import { theme, ThemeState } from './modules/theme';
 import { scroll, ScrollState } from './modules/scroll';
+import { preloader, PreloaderState } from './modules/preloader';
+import { burgerMenu, BurgerMenuState } from './modules/burgerMenu';
+
 
 export interface RootState 
 {
     theme: ThemeState;
     scroll: ScrollState;
+    preloader: PreloaderState;
+    burgerMenu: BurgerMenuState;
 }
 
 export const store = createStore<RootState>(
@@ -14,5 +19,7 @@ export const store = createStore<RootState>(
     {
         theme,
         scroll,
+        preloader,
+        burgerMenu
     },
 });
